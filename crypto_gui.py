9f08e5ae797609409b38e7291939de6ec063fb40
Neelang Pant <neelangpant2006@gmail.com>
2025-12-26 22:59:14 +0530

    Day3: Added auto-refresh , always-on-top, and built .exe

--- crypto_gui.py
+++ crypto_gui.py
@@ -1,34 +1,46 @@
 import customtkinter as ctk
 import requests
 
+previous_price = 0 #global variable
+
 def update_price():
+    global previous_price
     try:
         url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
         response = requests.get(url)
         data = response.json()
-        price = data['bitcoin']['usd']
+        current_price = data['bitcoin']['usd']
 
-        price_label.configure(text=f"${price:,}")
+        if current_price > previous_price:
+            colour = "#00FF00"
+            arrow = "▲"
+        elif current_price < previous_price:
+            colour = "#FF0000"
+            arrow = "▼"
+        else:
+            colour = "#FFFFFF"
+            arrow = "="
+        
+        price_label.configure(text=f"${arrow} ${current_price:,}", text_color = colour)
+        previous_price = current_price
+        print(f"Updated: ${current_price}")
 
-        print("Price updated")
     except Exception as e:
         price_label.configure(text="Error")
         print(e)
+    app.after(10000, update_price)
 
+#GUI Setup
 ctk.set_appearance_mode('dark')
-ctk.set_default_color_theme('blue')
-
 app = ctk.CTk()
-app.geometry("500x300")
-app.title("Crypto Tracker")
 
-title_label = ctk.CTkLabel(app, text = "Bitcoin price tracker", font=("Arial", 24, "bold"))
-title_label.pack(pady=20)
+app.title("BTC Tracker")
+app.geometry("200x70")
 
-price_label = ctk.CTkLabel(app, text="Press Refresh...", font=("Arial", 30), text_color="#00FF00")
+app.attributes('-topmost', True)
+
+price_label = ctk.CTkLabel(app, text="Loading....", font=("Arial", 32 , "bold"))
 price_label.pack(pady=20)
 
-refresh_btn = ctk.CTkButton(app, text="Refresh Price", command=update_price)
-refresh_btn.pack(pady=20)
-
+update_price()
 app.mainloop()
